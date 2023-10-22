79bd28914719210aa0247cc0e1facef390feaac2
AndreaOrca <202110209@gordoncollege.edu.ph>
2023-10-23 00:29:41 +0800

    login changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import './Login.css';
-import logo from "../assets/logos/gc-logo.png";
+import school from "../assets/gc_school.JPG";
 import LoginForm from '../components/LoginForm';
 
 export class Login extends Component {
     render() {
         return (
-            <div className="login_row">
-                <div className="form_container">
-                    <div className="form_content_left"><img src={logo} alt="GC Logo" /></div>
-                    <div className="form_content_right">
+            <div className="login_page_container">
+                <div class="left_section"></div>
+                <div class="right_section">
                     <LoginForm/>
-                    </div>
                 </div>
             </div>
         );
